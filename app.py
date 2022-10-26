0ec2201e87fdcd672329a790115732681827897c
Ejiro Ugo <ejaylyn98@gmail.com>
2022-10-26 18:00:04 +0100

    testing and testing

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-import pytest
+
 
 
 
@@ -10,31 +10,7 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', greeting="Hello!")
 
-@pytest.fixture
-def captured_template(app):
-    recorded = [] 
-
-    def record(sender, templates, context):
-        recorded.append((templates, context))
-
-    templates_rendered.connect(record, app)
-    try:
-        yield recorded
-    finally:
-        templates_rendered.disconnect(record, app)   
-
-
-def test(test_client, captured_template):
-    response = test_client.get('/home')
-
-    assert len(captured_template) == 1
-
-    templates, context = captured_template[0]
-
-    assert templates.name == 'home.html'
-    assert 'greeting' in context
-    assert context['greeting'] == 'Hello!'           
-
+          
 @app.route('/lead')
 def lead():
     return render_template('lead.html')
